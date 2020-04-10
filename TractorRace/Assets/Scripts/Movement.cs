e31456441bdf70327d25ba6ed2f618a2782c7b48
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2020-04-10 04:25:19 -0400

    updates
    You can now move the tractor.
    to correct
    (the base speed,
    polish the curves,
    add a plane to avoid friction
    )
    next update (
    i.a,
    lap times,
    add tractor details)

---
+++ TractorRace/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public WheelCollider WheelBL;
+    public WheelCollider WheelBR;
+    public WheelCollider WheelFR;
+    public WheelCollider WheelFL;
+    public int Velocity = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WheelFL.motorTorque = Velocity * Input.GetAxis("Vertical");
+        WheelFR.motorTorque = Velocity * Input.GetAxis("Vertical");
+
+        WheelBL.steerAngle = 10 * Input.GetAxis("Horizontal");
+        WheelBR.steerAngle = 10 * Input.GetAxis("Horizontal");
+    }
+}
