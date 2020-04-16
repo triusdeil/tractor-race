50d2d47fc8678f632efdd7d8458d3516db58c023
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2020-04-16 14:21:19 -0400

    Beta Game

---
+++ TractorRace/Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene (2);
+    }
+    public void TrackSelect(){
+        SceneManager.LoadScene (1);
+    }
+    public void MainMenu(){
+        SceneManager.LoadScene (0);
+    }
+
+    //Bellow here are track selection buttons
+    
+    public void Track01(){
+        SceneManager.LoadScene (2);
+        }
+}
